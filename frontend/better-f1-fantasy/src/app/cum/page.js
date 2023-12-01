b464b0427b1437e0bceb77e1b19b63955067f132
Julian Jones <julianjones663@gmail.com>
2023-12-01 18:06:09 +0000

    new pc

---
+++ frontend/better-f1-fantasy/src/app/cum/page.js
@@ -0,0 +1,12 @@
+'use client'
+
+import { Center } from "@chakra-ui/layout"
+
+
+export default function Home() {
+  return (
+    <Center width='calc(100vw)' height='calc(100vh)'>
+      WYS G?
+    </Center>
+  )
+}
