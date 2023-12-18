03b7f7da77701d820e95d40f0b95d6cab07a5d3e
Julian Jones <julianjones663@gmail.com>
2023-12-18 22:11:58 +0000

    centered footer

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -1,30 +1,30 @@
 export default function Footer() {
   return (
-    <footer class="fixed inset-x-0 bottom-0 bg-gray-800 rounded-t-lg">
-      <div class="w-full mx-0 max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-        <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">©2023 <a href="https://julians.work" class="hover:underline">NexInfinite</a>. All Rights Reserved.
+    <footer class="w-full fixed inset-x-0 bottom-0 bg-gray-800 rounded-t-lg flex justify-center">
+      <div class="w-screen mx-0 max-w-screen-xl p-4 md:flex items-center justify-between">
+        <span class="text-sm text-gray-400">©2023 <a href="https://julians.work" class="hover:underline">NexInfinite</a>. All Rights Reserved.
         </span>
-        <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <ul class="flex flex-wrap items-center text-sm font-medium text-gray-400 mt-0">
           <li>
-            <a href="/about" class="hover:underline me-4 md:me-6">About</a>
+            <a href="/about" className="hover:underline me-4 md:me-6">About</a>
           </li>
           <li>
-            <a href="https://github.com/nexinfinite" class="hover:underline me-4 md:me-6">Github</a>
+            <a href="https://github.com/nexinfinite" className="hover:underline me-4 md:me-6">Github</a>
           </li>
           <li>
-            <a href="/license" class="hover:underline me-4 md:me-6">Licensing</a>
+            <a href="/license" className="hover:underline me-4 md:me-6">Licensing</a>
           </li>
           <li>
-            <a href="mailto:julianjones663@gmail.com" class="hover:underline me-4 md:me-6">Contact</a>
+            <a href="mailto:julianjones663@gmail.com" className="hover:underline me-4 md:me-6">Contact</a>
           </li>
           <li>
-            <a href="/privacy-policy" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+            <a href="/privacy-policy" className="hover:underline me-4 md:me-6">Privacy Policy</a>
           </li>
           <li>
-            <a href="https://fantasy.formula1.com/en/" class="hover:underline">Original F1 Fantasy</a>
+            <a href="https://fantasy.formula1.com/en/" className="hover:underline">Original Fantasy</a>
           </li>
         </ul>
       </div>
     </footer>
   )
-} //TODO MOVE LINKS TO RIGHT
+}
---
+++ frontend/pages/leagues.js
@@ -0,0 +1,10 @@
+export default function Leagues() {
+  return (
+    <div>
+      <div className="text-center place-content-center">
+        <h1 className="text-6xl text-f1-red font-f1-bold">Leagues</h1>
+        <h2 className="text-xl text-dark-text font-f1-regular">TBF</h2>
+      </div>
+    </div>
+  )
+}
