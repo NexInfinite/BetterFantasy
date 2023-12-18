ba8cb7dc830cdde9c54ef2552bfc90e88b744122
Julian Jones <julianjones663@gmail.com>
2023-12-18 22:34:45 +0000

    made navbar fully responsive

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -1,27 +1,27 @@
 export default function Footer() {
   return (
     <footer class="w-full fixed inset-x-0 bottom-0 bg-gray-800 rounded-t-lg flex justify-center">
-      <div class="w-screen mx-0 max-w-screen-xl p-4 md:flex items-center justify-between">
-        <span class="text-sm text-gray-400">©2023 <a href="https://julians.work" class="hover:underline">NexInfinite</a>. All Rights Reserved.
+      <div class="w-screen mx-0 max-w-screen-xl p-4 flex flex-wrap items-center justify-stretch lg:justify-between">
+        <span class="text-sm text-gray-400 w-full lg:w-max text-center">©2023 <a href="https://julians.work" target="_blank" class="hover:underline">NexInfinite</a>. All Rights Reserved.
         </span>
-        <ul class="flex flex-wrap items-center text-sm font-medium text-gray-400 mt-0">
+        <ul class="flex flex-wrap justify-center w-full lg:w-max lg:items-center text-sm font-medium text-gray-400 mt-0">
           <li>
             <a href="/about" className="hover:underline me-4 md:me-6">About</a>
           </li>
           <li>
-            <a href="https://github.com/nexinfinite" className="hover:underline me-4 md:me-6">Github</a>
+            <a href="https://github.com/nexinfinite/betterf1fantasy" target="_blank" className="hover:underline me-4 md:me-6">Github</a>
           </li>
           <li>
             <a href="/license" className="hover:underline me-4 md:me-6">Licensing</a>
           </li>
           <li>
-            <a href="mailto:julianjones663@gmail.com" className="hover:underline me-4 md:me-6">Contact</a>
+            <a href="mailto:julianjones663@gmail.com" target="_blank" className="hover:underline me-4 md:me-6">Contact</a>
           </li>
           <li>
             <a href="/privacy-policy" className="hover:underline me-4 md:me-6">Privacy Policy</a>
           </li>
           <li>
-            <a href="https://fantasy.formula1.com/en/" className="hover:underline">Original Fantasy</a>
+            <a href="https://fantasy.formula1.com/en/" target="_blank" className="hover:underline">Original Fantasy</a>
           </li>
         </ul>
       </div>
