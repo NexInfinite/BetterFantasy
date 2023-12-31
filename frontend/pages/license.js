cc20c3a18b62e5c39f1bad5f0c6e1bfb851a8c77
Julian Jones <julianjones663@gmail.com>
2023-12-31 02:27:40 +0000

    updated footer

---
+++ frontend/pages/license.js
@@ -0,0 +1,13 @@
+import Layout from "@/components/layout"
+
+export default function Licensing() {
+    return (
+      <Layout title="Licensing" description="Our Licensing Policy">
+        <div id="page-heading" className="text-center">
+          <h1 className="text-6xl text-f1-red font-f1-bold">Licensing</h1>
+          <h2 className="text-xl text-dark-text font-f1-regular">To be figured out</h2>
+        </div>
+      </Layout>
+    )
+  }
+  
---
+++ frontend/pages/privacy.js
@@ -0,0 +1,12 @@
+import Layout from "@/components/layout"
+
+export default function Privacy() {
+  return (
+    <Layout title="Privacy" description="Our Privacy Policy">
+      <div id="page-heading" className="text-center">
+        <h1 className="text-6xl text-f1-red font-f1-bold">Privacy</h1>
+        <h2 className="text-xl text-dark-text font-f1-regular">TBF</h2>
+      </div>
+    </Layout>
+  )
+}
