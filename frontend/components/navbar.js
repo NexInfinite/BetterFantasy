ee71d02bee44ad8d184c9ff54d4223b7f12d670b
Julian Jones <julianjones663@gmail.com>
2023-12-31 02:14:03 +0000

    added links in main

--- frontend/components/navbar.js
+++ frontend/components/navbar.js
@@ -91,23 +91,23 @@ export default function NavBar() {
                       <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <Menu.Item>
                           {({ active }) => (
-                            <a href="#" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                            <Link href="#" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
                               Your Profile
-                            </a>
+                            </Link>
                           )}
                         </Menu.Item>
                         <Menu.Item>
                           {({ active }) => (
-                            <a href="#" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                            <Link href="#" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
                               Settings
-                            </a>
+                            </Link>
                           )}
                         </Menu.Item>
                         <Menu.Item>
                           {({ active }) => (
-                            <a href="#" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                            <Link href="#" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
                               Sign out
-                            </a>
+                            </Link>
                           )}
                         </Menu.Item>
                       </Menu.Items>
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,3 +1,4 @@
+import Link from "next/link"
 import Layout from "@/components/layout"
 
 export default function Home() {
@@ -31,7 +32,7 @@ export default function Home() {
           <div className="p-4 bg-slate-100 rounded-lg">
             <h3 className="text-xl text-f1-red font-f1-bold sm:text-4xl text-left">2. </h3>
             <h4 className="text-m text-dark-text font-f1-bold sm:text-2xl text-left">Join or Make a League</h4>
-            <p className="text-xs text-dark-text font-f1-regular sm:text-s text-left">Once your account has been created, join or make a league by pressing the "Leagues" link on the right of the navbar</p>
+            <p className="text-xs text-dark-text font-f1-regular sm:text-s text-left">Once your account has been created, join or make a league by pressing the "<Link href="/leagues" scroll={false} className="transition-color ease-out underline underline-offset-2 hover:text-f1-red hover:bg-red-100">Leagues</Link>" link on the right of the navbar</p>
           </div>
           <div className="p-4 bg-slate-100 rounded-lg">
             <h3 className="text-xl text-f1-red font-f1-bold sm:text-4xl text-left">3. </h3>
@@ -41,7 +42,7 @@ export default function Home() {
           <div className="p-4 bg-slate-100 rounded-lg">
             <h3 className="text-xl text-f1-red font-f1-bold sm:text-4xl text-left">4. </h3>
             <h4 className="text-m text-dark-text font-f1-bold sm:text-2xl text-left">Create Your Team</h4>
-            <p className="text-xs text-dark-text font-f1-regular sm:text-s text-left">Once your league has been customized, you can now create your team! Head to the "Teams" section on the navbar and follow the steps show</p>
+            <p className="text-xs text-dark-text font-f1-regular sm:text-s text-left">Once your league has been customized, you can now create your team! Head to the "<Link href="/teams" scroll={false} className="transition-color ease-out underline underline-offset-2 hover:text-f1-red hover:bg-red-100">Teams</Link>" section on the navbar and follow the steps show</p>
           </div>
           <div className="p-4 bg-slate-100 rounded-lg">
             <h3 className="text-xl text-f1-red font-f1-bold sm:text-4xl text-left">5. </h3>
