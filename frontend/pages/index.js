7c93c3b5f2b3ca6db7e0ba0b368a1ce4a8f8f905
Julian Jones <julianjones663@gmail.com>
2024-01-02 00:24:32 +0000

    going to use folders now

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,7 +3,7 @@ import Layout from "@/components/layout"
 
 export default function Home() {
   return (
-    <Layout title="Home" description="Better F1 Fantasy Home!">
+    <Layout title="Home" description="Better Fantasy Home">
       <div id="page-heading" className="text-center">
         <h1 className="text-4xl text-f1-red font-f1-bold sm:text-6xl">F1 FANTASY</h1>
         <h2 className="text-m text-dark-text font-f1-regular sm:text-xl">An Unofficial Fantasy League</h2>
--- frontend/pages/teams.js
+++ frontend/pages/teams.js
@@ -2,7 +2,7 @@ import Layout from "@/components/layout"
 
 export default function Teams() {
   return (
-    <Layout title="Teams" description="Your F1 Fantasy Teams">
+    <Layout title="Teams" description="Your Fantasy Teams">
       <div id="page-heading" className="text-center">
         <h1 className="text-6xl text-f1-red font-f1-bold">Teams</h1>
         <h2 className="text-xl text-dark-text font-f1-regular">TBF</h2>
