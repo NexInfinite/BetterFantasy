3cad510e815b43e0ff08a3467b00abf111386377
Julian Jones <julianjones663@gmail.com>
2024-01-08 01:02:40 +0000

    changed margins

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -4,14 +4,12 @@ import {LoginForm} from "@/components/login"
 export default function Login() {
     return (
       <Layout title="Login" description="Login or create an account!">
-        <div className="h-50 w-full flex flex-col justify-center">
-          <div>
+        <div className="mt-12">
             <div id="page-heading" className="text-center">
               <h1 className="text-6xl text-main font-theme-bold">Login</h1>
               <h2 className="text-xl text-dark-text font-theme-regular">Welcome back</h2>
             </div>
             <LoginForm />
-          </div>
         </div>
       </Layout>
     )
