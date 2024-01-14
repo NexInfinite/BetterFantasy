8d2168d7e7d508d232cbf68ca7482ddbeda78ea2
Julian <julianjones663@gmail.com>
2024-01-14 16:22:06 +0000

    made login page actually look nice

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -4,13 +4,11 @@ import {LoginForm} from "@/components/login"
 export default function Login() {
     return (
       <Layout title="Login" description="Login or create an account!">
-        <div className="mt-12">
-            <div id="page-heading" className="text-center">
-              <h1 className="text-6xl text-main font-theme-bold">Login</h1>
-              <h2 className="text-xl text-dark-text font-theme-regular">Welcome back</h2>
-            </div>
-            <LoginForm />
-        </div>
+          <div id="page-heading" className="text-center">
+            <h1 className="text-6xl text-main font-theme-bold">Login</h1>
+            <h2 className="text-xl text-dark-text font-theme-regular">Welcome back</h2>
+          </div>
+          <LoginForm />
       </Layout>
     )
   }
