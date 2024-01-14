a7d3d44a3658a6bab5fb69b21261614b539537e4
Julian <julianjones663@gmail.com>
2024-01-14 16:40:54 +0000

    basic transition working

---
+++ frontend/components/loading.js
@@ -0,0 +1,25 @@
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+
+export default function Loading() {
+  const router = useRouter();
+
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+      const handleStart = (url) => (url !== router.asPath) && setLoading(true);
+      const handleComplete = (url) => (url === router.asPath) && setLoading(false);
+
+      router.events.on('routeChangeStart', handleStart)
+      router.events.on('routeChangeComplete', handleComplete)
+      router.events.on('routeChangeError', handleComplete)
+
+      return () => {
+          router.events.off('routeChangeStart', handleStart)
+          router.events.off('routeChangeComplete', handleComplete)
+          router.events.off('routeChangeError', handleComplete)
+      }
+  })
+  
+  return loading && (<div>Loading....</div>);
+}
