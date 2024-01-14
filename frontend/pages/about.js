2432d6e481ccd82d6099d6132b6d670e5bb59267
Julian <julianjones663@gmail.com>
2024-01-14 22:02:21 +0000

    fixed index not being found

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,13 +1,13 @@
 import Layout from "@/components/layout"
 
 export default function About() {
-    return (
-      <Layout title="About" description="What about it? Oh, about Better Fantasy">
-        <div id="page-heading" className="text-center">
-          <h1 className="text-6xl text-main font-theme-bold">About</h1>
-          <h2 className="text-xl text-dark-text font-theme-regular">Some Information</h2>
-        </div>
-      </Layout>
-    )
-  }
+  return (
+    <Layout title="About" description="What about it? Oh, about Better Fantasy">
+      <div id="page-heading" className="text-center">
+        <h1 className="text-6xl text-main font-theme-bold">About</h1>
+        <h2 className="text-xl text-dark-text font-theme-regular">Some Information</h2>
+      </div>
+    </Layout>
+  )
+}
   
