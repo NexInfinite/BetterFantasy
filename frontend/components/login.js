cfd1e007515ae577346b0aac9ca8a77ef15d92e7
Julian <julianjones663@gmail.com>
2024-01-15 20:43:18 +0000

    added or section for create account

--- frontend/components/login.js
+++ frontend/components/login.js
@@ -1,45 +1,68 @@
 export const LoginForm = () => (
   <div className="flex w-full justify-center mt-6 font-theme-regular">
-    <form className="w-10/12 max-w-xl bg-white rounded-lg shadow-xl border-slate-100 border-solid border-2 py-12 px-10 bg-main">
-      <label htmlFor="email" className="block text-main text-lg font-bold mb-2">
-        Email
-      </label>
-      <input
-        id="email"
-        type="email"
-        className="w-full text-sm appearance-none rounded-md ring-2 ring-inset ring-main bg-white py-2 px-3 leading-tight text-main focus:outline-0 italic placeholder:text-neutral-400 placeholder:text-sm"
-        placeholder="email@domain.com"
-        pattern="[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
-        title="Invalid Email Address"
-      />
+    <div className="w-10/12 max-w-xl bg-white rounded-lg shadow-xl border-slate-100 border-solid border-2 p-10 bg-main">
 
-      <label
-        htmlFor="password"
-        className="block text-main text-lg font-bold mb-2 mt-6"
-      >
-        Password
-      </label>
-      <input
-        id="password"
-        type="password"
-        className="w-full text-sm appearance-none rounded-md ring-2 ring-inset ring-main bg-white py-2 px-3 leading-tight text-main focus:outline-0 italic placeholder:text-neutral-400 placeholder:text-sm"
-        placeholder="password"
-      />
+      <form>
+        {/* Email Input */}
+        <label htmlFor="email" className="block text-main text-lg font-bold mb-2">
+          Email
+        </label>
+        <input
+          id="email"
+          type="email"
+          className="w-full text-sm appearance-none rounded-md ring-2 ring-inset ring-main bg-white py-2 px-3 leading-tight text-main focus:outline-0 italic placeholder:text-neutral-400 placeholder:text-sm"
+          placeholder="email@domain.com"
+          pattern="[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"
+          title="Invalid Email Address"
+        />
 
-      <div className="flex items-center justify-between mt-6">
-        <button
-          className="transition-all ease-in duration-400 ring-2 ring-inset ring-main text-main font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline text-sm md:text-md hover:text-white hover:bg-main"
-          type="submit"
+        {/* Password Input */}
+        <label
+          htmlFor="password"
+          className="block text-main text-lg font-bold mb-2 mt-6"
         >
-          Sign In
-        </button>
-        <a
-          className="inline-block align-baseline underline text-xs sm:text-sm text-main hover:decoration-2"
-          href="/forgot-password"
-        >
-          Forgot Password?
-        </a>
+          Password
+        </label>
+        <input
+          id="password"
+          type="password"
+          className="w-full text-sm appearance-none rounded-md ring-2 ring-inset ring-main bg-white py-2 px-3 leading-tight text-main focus:outline-0 italic placeholder:text-neutral-400 placeholder:text-sm"
+          placeholder="password"
+        />
+
+        {/* Login Button/Forgot Password */}
+        <div className="flex items-center justify-between mt-6">
+          <button
+            className="transition-all ease-in duration-400 ring-2 ring-inset ring-main text-main font-bold py-2 px-4 rounded-md focus:outline-none focus:shadow-outline text-sm md:text-md hover:text-white hover:bg-main"
+            type="submit"
+          >
+            Sign In
+          </button>
+          <a
+            className="inline-block align-baseline underline text-xs sm:text-sm text-main hover:decoration-2"
+            href="/forgot-password"
+          >
+            Forgot Password?
+          </a>
+        </div>
+
+      </form> 
+
+      {/* Or */}
+      <div className="relative flex py-2 items-center">
+        <div className="flex-grow border-t border-gray-400"></div>
+        <span className="flex-shrink mx-4 text-gray-400">or</span>
+        <div className="flex-grow border-t border-gray-400"></div>
       </div>
-    </form>
+
+      {/* Create Account */}
+      <div className="text-center">
+        <p>New to Better Fantasy?</p>
+        <button className="transition-all ease-in duration-400 ring-2 ring-inset ring-main text-main font-bold mt-2 py-2 px-4 rounded-md focus:outline-none focus:shadow-outline text-sm md:text-md hover:text-white hover:bg-main">
+          Create Account
+        </button>
+      </div>
+    </div>
+
   </div>
 );
